0679c4ebb445057ce3b0e334f117de5c5d263bd8
Sébastien Foutrier <sebastien.foutrier@gmail.com>
2017-07-30 11:32:42 +0200

    Initial commit

---
+++ Peano/Form1.cs
@@ -0,0 +1,43 @@
+﻿using PeanoGenerator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int iteration = 1;
+            int patternSize = 3;
+            int edgeLength = 10;
+            int imgSize = (int)Math.Pow(patternSize,(iteration + 1 )) * edgeLength;
+            _fractal = new Bitmap(imgSize, imgSize);
+            using (var g = Graphics.FromImage(_fractal))
+            {
+                var pen = new Pen(Color.Black);
+                var points = Stuff.GetPeanoPatten(iteration, edgeLength);
+                g.DrawLines(pen, points.ToArray());
+            }
+
+            pictureBox1.SizeChanged += PictureBox1_SizeChanged;
+            PictureBox1_SizeChanged(null, null);
+        }
+
+        private void PictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(_fractal, pictureBox1.ClientSize);
+        }
+
+        private Image _fractal;
+    }
+}
---
+++ PeanoGenerator/Stuff.fs
@@ -0,0 +1,42 @@
+﻿namespace PeanoGenerator
+
+open System.Drawing
+
+module Stuff =
+    // defines the directions to move to draw the picture
+    type Directions = Left | Right | Up | Down
+
+    // the pattern that start the fractal
+    let basePattern = [Down; Down; Right; Up; Up; Right; Down; Down]
+
+    // trasformation that applies on the previous pattern when moving from a tile to the next
+    let flipHorizontaly = List.rev >> (List.map (function Left -> Right | Right -> Left | d -> d))
+    let flipVertically = List.rev >> (List.map (function Up -> Down | Down -> Up | d -> d))
+    let flip = function | Up | Down -> flipHorizontaly | Left | Right -> flipVertically
+
+    // an iteration run: replicates the base pattern to all previous steps
+    // and transform it on each steps, then append the n-1 direction
+    let iterate s =
+        Seq.zip (Seq.scan (fun prevPattern d -> flip d prevPattern) basePattern s) s
+        |> Seq.map (fun (s,d) -> s @ [d]) |> Seq.concat
+
+    // reapplies the iteration the given number of time
+    let fractal i =
+        let rec fractal_i i pattern =
+            match i with 0 -> pattern | i -> fractal_i (i-1) (iterate pattern)
+        fractal_i i basePattern
+
+    // converts from directions to points to join starting from top-left corner
+    let toPoints edgeLegth =
+        Seq.scan
+            (fun position direction ->
+                match direction with
+                | Left -> position + Size(-edgeLegth, 0)
+                | Right -> position + Size(edgeLegth, 0)
+                | Up -> position + Size(0, -edgeLegth)
+                | Down -> position + Size(0, edgeLegth)
+            ) (Point(0,0))
+
+    // Ze public API
+    let GetPeanoPatten (iteration: int, edgeLength: int) =
+        fractal iteration |> toPoints edgeLength
